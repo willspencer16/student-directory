1805fffb6e0b70bff32bcc37fcc379d6055668f8
Will Spencer <willspencer16@gmail.com>
2021-02-23 11:23:34 +0000

    Created a copy directory and added modified the program to print a number before each user input

---
+++ directory_copy.rb
@@ -0,0 +1,34 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do 
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
