db3b873494388f5e7f9b7539b08d234cf607fff7
Will Spencer <willspencer16@gmail.com>
2021-02-24 11:39:50 +0000

    Added interactive menu and the ability to save the data to a file

--- directory.rb
+++ directory.rb
@@ -1,16 +1,53 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
   name = gets.chomp
 
-  while !name.empty? do 
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+  while !name.empty? do
+
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+
     name = gets.chomp
   end
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 def print_header
@@ -18,17 +55,25 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+def save_students
+  file = File.open("students.csv", "w")
+
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+    end
+    file.close
+  end
+
+interactive_menu
