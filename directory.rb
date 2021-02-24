19216e462c41cc3129205881f26341497c754a9c
Will Spencer <willspencer16@gmail.com>
2021-02-24 14:39:51 +0000

    Added thhe ability to take agruments from the command line.

--- directory.rb
+++ directory.rb
@@ -1,48 +1,18 @@
 @students = []
 
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-
-  name = gets.chomp
-
-  while !name.empty? do
-
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-
-    name = gets.chomp
-  end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list to students.csv"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print_student_list
-  print_footer
 end
 
 def process(selection)
@@ -51,15 +21,34 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "9"
+    exit
   when "3"
     save_students
   when "4"
     load_students
-  when "9"
-    exit
   else
     puts "I don't know what you meant, try again"
   end
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  name = STDIN.gets.chomp
+
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def print_header
@@ -84,8 +73,30 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
-    end
-    file.close
   end
+  file.close
+end
 
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+try_load_students
 interactive_menu
