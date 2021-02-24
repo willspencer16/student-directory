cab222080aa99c2c5faa2978d1ee8678036224f6
Will Spencer <willspencer16@gmail.com>
2021-02-24 14:26:59 +0000

    Added ability to load data from the file.

--- directory.rb
+++ directory.rb
@@ -1,5 +1,14 @@
 @students = []
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -26,6 +35,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list to students.csv"
   puts "9. Exit"
 end
 
@@ -43,6 +53,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
